495db8a92029bcb36f352e229da4ca697e5964c9
mundus work <raymon.prijati@gmail.com>
2019-09-04 19:02:11 +0700

    test

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Inventory;
-use Yajra\Datatables\Facades\Datatables;
+use Yajra\Datatables\Datatables;
 
 class InventoryController extends Controller
 {
